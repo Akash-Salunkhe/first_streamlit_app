fa2af9a797419bd636d8cafe520198f3046bb46e
Akash-Salunkhe <72498844+Akash-Salunkhe@users.noreply.github.com>
2023-10-08 01:59:41 +0530

    Update streamlit_app.py
    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"]) -> 
    my_cnx = snowflake.connector.connect(**dict(streamlit.secrets)["snowflake"])

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 streamlit.text(streamlit.secrets['snowflake'])
 streamlit.text(type(dict(streamlit.secrets)))
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**dict(streamlit.secrets)["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
