9e0803b77360e5743daa22d513b423f2abf8e576
Akash-Salunkhe <72498844+Akash-Salunkhe@users.noreply.github.com>
2023-10-08 03:32:07 +0530

    Update streamlit_app.py
    removed the hard coded value 'from streamlit' from insert statement and closed the connections

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,12 +56,14 @@ def get_fruit_load_list():
 if streamlit.button('Get fruit load list'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 # Allow the user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
-    my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
+    my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('" + new_fruit + "')")
+    my_cnx.close()
     return "Thanks for adding " + new_fruit
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
